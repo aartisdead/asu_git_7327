8e864e2692d4140df217952a5f72b45253429596
lena <aartisdead@gmail.com>
2019-04-07 16:04:37 +0300

    shape

--- game.js
+++ game.js
@@ -635,15 +635,13 @@ AlienBullet.prototype = new Bullet();
 Asteroid = function () {
   this.init("asteroid",
             [-10,   0,
-              -5,   7,
-              -3,   4,
-               1,  10,
-               5,   4,
-              10,   0,
-               5,  -6,
-               2, -10,
-              -4, -10,
-              -4,  -5]);
+              -12,   -4,
+              12,   -4,
+               20,  0,
+               12,   4,
+              -12,   4,
+               -10,  0,
+               20,   0]);
 
   this.visible = true;
   this.scale = 6;
